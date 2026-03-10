723fc7417899face23957f445b8a6168e1a52a35
gong1035426184 <gong1035426184@gmail.com>
2026-03-10 21:58:48 +0800

    Create Album.kt

---
+++ src/main/kotlin/com/github/eprendre/tingshu/model/Album.kt
@@ -0,0 +1,10 @@
+package com.github.eprendre.tingshu.model
+
+data class Album(
+    val name: String,
+    val coverUrl: String,
+    val intro: String,
+    val detailUrl: String,
+    val author: String,
+    val artist: String
+)
